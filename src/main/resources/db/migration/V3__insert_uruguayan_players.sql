50c08105b71eff67dbcd7c7828559aa3415c1532
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-04-20 22:57:14 -0300

    feat: add migrations that create initial tables and populate the tables with initial data

---
+++ src/main/resources/db/migration/V3__insert_uruguayan_players.sql
@@ -0,0 +1,23 @@
+INSERT INTO player (id, name, position, shirt_number, club_id) VALUES
+    (nextval('player_seq'), 'Luis Suárez', 'FORWARD', 9, 1),
+    (nextval('player_seq'), 'Rodrigo Erramuspe', 'DEFENDER', 2, 1);
+
+INSERT INTO player (id, name, position, shirt_number, club_id) VALUES
+    (nextval('player_seq'), 'Facundo Torres', 'MIDFIELDER', 10, 2),
+    (nextval('player_seq'), 'Kevin Dawson', 'GOALKEEPER', 1, 2);
+
+INSERT INTO player (id, name, position, shirt_number, club_id) VALUES
+    (nextval('player_seq'), 'Álvaro Navarro', 'FORWARD', 7, 3),
+    (nextval('player_seq'), 'Emiliano Álvarez', 'DEFENDER', 4, 3);
+
+INSERT INTO player (id, name, position, shirt_number, club_id) VALUES
+    (nextval('player_seq'), 'Juan Ignacio Ramírez', 'FORWARD', 11, 4),
+    (nextval('player_seq'), 'Camilo Cándido', 'MIDFIELDER', 6, 4);
+
+INSERT INTO player (id, name, position, shirt_number, club_id) VALUES
+    (nextval('player_seq'), 'Santiago Mederos', 'MIDFIELDER', 8, 5),
+    (nextval('player_seq'), 'Mauro Goicoechea', 'GOALKEEPER', 1, 5);
+
+INSERT INTO player (id, name, position, shirt_number, club_id) VALUES
+    (nextval('player_seq'), 'Federico Valverde', 'MIDFIELDER', 15, 6),
+    (nextval('player_seq'), 'Ronald Araújo', 'DEFENDER', 4, 6);
