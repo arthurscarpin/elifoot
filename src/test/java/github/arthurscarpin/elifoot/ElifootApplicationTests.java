0abe7d5e06d5e4ddd5119d0edf6ef172c14cf2a3
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-04-20 22:15:00 -0300

    chore: initial project

---
+++ src/test/java/github/arthurscarpin/elifoot/ElifootApplicationTests.java
@@ -0,0 +1,13 @@
+package github.arthurscarpin.elifoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElifootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
